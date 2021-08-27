f04ecf8035eb64217f03ee85d78a76bb2d9fde1e
GitHub Action <action@github.com>
2021-08-27 15:13:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/tuoaitang/myfirstplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tuoaitang/myfirstplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tuoaitang.myfirstplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tuoaitang.myfirstplugin.MyBundle
 
 class MyApplicationService {
 
